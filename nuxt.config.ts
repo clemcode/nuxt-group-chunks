3246ac541807fc74108029a8c82d054bdeb25973
Clément Ollivier <clement.o2p@gmail.com>
2023-03-16 14:33:42 +0100

    chore: remove unused code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  groupChunks: {
-    directories: [
-      {
-        path: 'components/app',
-        chunkName: 'app-common'
-      },
-      {
-        path: 'components/cart',
-        chunkName: 'cart-common'
-      }
-    ]
-  }
+  
 })
